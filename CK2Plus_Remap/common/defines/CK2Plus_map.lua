f953ccf32391c396179fe79e037d2ccac7321ec3
Ivan Vuk <ivan.vuk33@gmail.com>
2017-09-20 19:43:03 +0200

    FIX: main menu map position

---
+++ CK2Plus_Remap/common/defines/CK2Plus_map.lua
@@ -0,0 +1,7 @@
+--Main menu position
+NDefines.NFrontend.FRONTEND_POS_X = 1050.0
+NDefines.NFrontend.FRONTEND_POS_Y = 800.0
+NDefines.NFrontend.FRONTEND_POS_Z = 900.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 1050.0
+NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1100.0
